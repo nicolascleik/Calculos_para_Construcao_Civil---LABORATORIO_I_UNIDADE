d827277a3ab2fa0f631ab7f98dc986ebfe7d5b70
Nicolas Cleik <nicolas@cleik.dev>
2026-03-24 13:46:15 -0300

    Criação de funções calcular_area_em_m2 e calcular_tijolos_por_parede

--- app/calculadora_obras.py
+++ app/calculadora_obras.py
@@ -1,9 +1,9 @@
-class CalculadoraObras:
-    def __init__(self):
-        pass
+def calcular_area_em_m2(altura, comprimento):
+    area_m2 = altura * comprimento
 
-    def calcular_area_em_m2(self, altura, comprimento):
-        pass
+    return area_m2
 
-    def calcular_tijolos_por_parede(self, area_parede_m2, area_tijolo_m2):
-        pass
+def calcular_tijolos_por_parede(area_parede_m2, area_tijolo_m2):
+    quantidade_de_blocos = round((area_parede_m2 / area_tijolo_m2), 2)
+
+    return quantidade_de_blocos
