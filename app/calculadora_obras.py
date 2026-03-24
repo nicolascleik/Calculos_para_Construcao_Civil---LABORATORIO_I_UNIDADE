58b8a8530847cf0b34c31036023b3e3b05650ea4
Nicolas Cleik <nicolas@cleik.dev>
2026-03-24 13:36:09 -0300

    Incialização de pastas e estrutura do projeto

---
+++ app/calculadora_obras.py
@@ -0,0 +1,9 @@
+class CalculadoraObras:
+    def __init__(self):
+        pass
+
+    def calcular_area_em_m2(self, altura, comprimento):
+        pass
+
+    def calcular_tijolos_por_parede(self, area_parede_m2, area_tijolo_m2):
+        pass
